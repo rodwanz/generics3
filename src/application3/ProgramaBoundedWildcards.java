f3f24bbb09a12d6594fb24b8aba200785fa99899
Rodrigo Wanzeler <wanzervai@gmail.com>
2021-11-19 22:12:35 -0300

    Final implementation

--- src/application3/ProgramaBoundedWildcards.java
+++ src/application3/ProgramaBoundedWildcards.java
@@ -15,11 +15,15 @@ public class ProgramaBoundedWildcards {
 		myShapes.add(new Rectangle(3.0, 2.0));
 		myShapes.add(new Circle(2.0));
 		
+		List<Circle> myCircles = new ArrayList<>();
+		myCircles.add(new Circle( 2.0));
+		myCircles.add(new Circle(3.0));
+		
 		System.out.println("Total area: " + totalArea(myShapes));
 
 	}
 	
-	public static double totalArea(List<Shape> list) {
+	public static double totalArea(List<? extends Shape> list) {
 		double sum = 0.0;
 		for(Shape s : list) {
 			sum += s.area();
